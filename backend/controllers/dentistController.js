1bb05549b92d24b911b3b284b87752716678c1a6
sn00pcode <payoyodesiree@gmail.com>
2026-02-24 20:32:15 +0000

    add deleteDentist functionality

--- backend/controllers/dentistController.js
+++ backend/controllers/dentistController.js
@@ -1,6 +1,12 @@
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
-// const Dentist = require("../models/Dentist");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Dentist = require("../models/Dentist");
+
+// 201 create success
+// 200 read/update/delete success
+// 400 bad input / invalid id
+// 404 not found
+// 500 server error
 
 // // --------------------------CREATE DENTIST------------------------------
 // exports.createDentist = async (req, res) => {
@@ -8,3 +14,25 @@
 //         const { name, email, password, speciality } = req.body;
 //     }
 // }
+
+// --------------------------DELETE DENTIST------------------------------
+// validate ObjectId, delete by Id, return 404 if not found
+exports.deleteDentist = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid user id" });
+        }
+
+        const deletedDentist = await User.findByIdAndDelete(id);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json({ message: "User deleted successfully" });
+    } catch (err) {
+        return res.status(500).json({ message: "Server error" });
+    }
+}
