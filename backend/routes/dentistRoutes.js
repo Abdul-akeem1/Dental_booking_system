1071a2e61b5752925875c9b6be290c211741a1ee
sn00pcode <payoyodesiree@gmail.com>
2026-02-26 19:11:02 +0000

    add routes

--- backend/routes/dentistRoutes.js
+++ backend/routes/dentistRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const dentistController = require("../controllers/dentistController");
 
 router.delete("/:id", dentistController.deleteDentist);
+router.get("/", dentistController.getAllDentists);
+router.get("/:id", dentistController.getDentistById);
 
 module.exports = router;
