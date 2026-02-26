44f912213185b21aaf694eccfc217fbb99ea2423
desiree1002 <payoyodesiree@gmail.com>
2026-02-26 14:51:57 +0000

    Add user reference to treatment schema

--- backend/models/Treatment.js
+++ backend/models/Treatment.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const treatmentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: True  },
   amount: { type: Number, required: true },
   dateIssued: { type: Date, default: Date.now },
   cost: { type: Number, required: true}
