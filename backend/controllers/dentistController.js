5139323ef985f6bc3903d35bfc52895c7912167f
sn00pcode <payoyodesiree@gmail.com>
2026-02-26 19:04:50 +0000

    add getAllDentist and getDentistById functionality

--- backend/controllers/dentistController.js
+++ backend/controllers/dentistController.js
@@ -15,6 +15,44 @@ const Dentist = require("../models/Dentist");
 //     }
 // }
 
+
+
+
+// -------------------------GET ALL DENTIST--------------------------
+exports.getAllDentists = async (req, res) => {
+    try {
+        const dentists = await Dentist.find({})
+        .select("-password")
+        .sort({ createdAt: -1 });
+        
+        return res.status(200).json({
+            count: dentists.length,
+            dentists,
+        });
+    } catch (err) {
+        return res.status(500).json({ message: "Server error"})
+    }
+}
+
+
+// -------------------------GET DENTIST BY ID------------------------------
+exports.getDentistById = async (req, res) => {
+    try{
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid user id" });
+        }
+        
+        const dentist = await Dentist.findById(id).select("-password");
+
+        if (!dentist) {
+            return res.status(404).json({ message: "User not found" });
+        }
+    }
+}
+
+
 // --------------------------DELETE DENTIST------------------------------
 // validate ObjectId, delete by Id, return 404 if not found
 exports.deleteDentist = async (req, res) => {
