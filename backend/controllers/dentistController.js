c0e2ddb2d3dfdca8f96a3bd4bb1385e612e905ed
sn00pcode <payoyodesiree@gmail.com>
2026-02-26 19:07:33 +0000

    close catch

--- backend/controllers/dentistController.js
+++ backend/controllers/dentistController.js
@@ -49,6 +49,10 @@ exports.getDentistById = async (req, res) => {
         if (!dentist) {
             return res.status(404).json({ message: "User not found" });
         }
+
+        return res.status(200).json(dentist);
+    } catch (err) {
+        return res.status(500).json({ message: "Server error" });
     }
 }
 
