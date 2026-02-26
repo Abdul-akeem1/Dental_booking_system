e5d039caf1895a6b09f0a0897a01b03558a34e24
sn00pcode <payoyodesiree@gmail.com>
2026-02-26 20:49:53 +0000

    add css

--- admin/src/pages/AddClient.jsx
+++ admin/src/pages/AddClient.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { useState } from "react";
 import axios from "axios";
+import "./AddClient.css";
 
 const AddClient = () => {
     const [firstName, setFirstName] = useState("");
@@ -45,11 +46,11 @@ const AddClient = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-          <div>
+        <form onSubmit={handleSubmit} className="add-client-form">
+          <div className="form-header">
             <h2>Add Client</h2>
           </div>
-          <div>
+          <div className="upload-section">
             <label htmlFor="den-img">
               <img src={assets.upload_area} alt="" />
             </label>
@@ -59,79 +60,89 @@ const AddClient = () => {
             </p>
           </div>
     
-          <div>
+          <div className="form-content">
             <div>
-              <div>
-                <p>Client First Name</p>
-                <input 
-                    type="text"
-                    placeholder="First Name"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)} 
-                />
-              </div>
+                <div className="container">
+                    <div className="input">
+                        <p>First Name</p>
+                        <input 
+                            type="text"
+                            placeholder="First Name"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)} 
+                        />
+                    </div>
 
-              <div>
-                <p>Client Last Name</p>
-                <input 
-                    type="text"
-                    placeholder="Last Name"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)} 
-                />
-              </div>
-
-              <div>
-                <p>Phone</p>
-                <input 
-                    type="text"
-                    placeholder="Phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)} 
-                />
-              </div>
-
-              <div>
-                <p>Email</p>
-                <input 
-                    type="text"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} 
-                />
-              </div>
-
-              <div>
-                <p>Street</p>
-                <input 
-                    type="text"
-                    placeholder="Street"
-                    value={street}
-                    onChange={(e) => setStreet(e.target.value)} 
-                />
-              </div>
+                    <div className="input">
+                        <p>Last Name</p>
+                        <input 
+                            type="text"
+                            placeholder="Last Name"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)} 
+                        />
+                    </div>
+                </div>
               
-              <div>
-                <p>Town</p>
-                <input 
-                    type="text"
-                    placeholder="Town"
-                    value={town}
-                    onChange={(e) => setTown(e.target.value)} 
-                />
+
+              <div className="container">
+                <div className="input">
+                    <p>Phone</p>
+                    <input 
+                        type="text"
+                        placeholder="Phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)} 
+                    />
+                </div>
+                
+                <div className="input">
+                    <p>Email</p>
+                    <input 
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                </div>
               </div>
 
-              <div>
-                <p>County</p>
-                <input 
-                    type="text"
-                    placeholder="County"
-                    value={county}
-                    onChange={(e) => setCounty(e.target.value)} 
-                />
-              </div>
+              
 
-              <div>
+                <div className="container">
+                    <div className="address-container">
+                        <p>Street</p>
+                        <input 
+                            type="text"
+                            placeholder="Street"
+                            value={street}
+                            onChange={(e) => setStreet(e.target.value)} 
+                        />
+                    </div>
+                    
+                    <div className="address-container">
+                        <p>Town</p>
+                        <input 
+                            type="text"
+                            placeholder="Town"
+                            value={town}
+                            onChange={(e) => setTown(e.target.value)} 
+                        />
+                    </div>
+
+                    <div className="address-container">
+                        <p>County</p>
+                        <input 
+                            type="text"
+                            placeholder="County"
+                            value={county}
+                            onChange={(e) => setCounty(e.target.value)} 
+                        />
+                    </div>
+                </div>
+              
+
+              <div className="input password-input">
                 <p>Password</p>
                 <input 
                     type="password"
@@ -141,10 +152,10 @@ const AddClient = () => {
                 />
               </div>
 
-              <div>
+              <div className="button">
                 <button type="submit">Create Client</button>
               </div>
-              
+
             </div>
           </div>
         </form>
